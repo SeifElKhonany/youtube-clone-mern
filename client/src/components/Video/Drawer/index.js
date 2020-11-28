b740d6b4887a04fe89065fce9dbdac7c41adabbd
Seif-Amgad <seifelkhonany@gmail.com>
2020-11-28 18:42:45 +0200

    Extracted Modal component

--- client/src/components/Video/Drawer/index.js
+++ client/src/components/Video/Drawer/index.js
@@ -10,84 +10,83 @@ import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import MenuIcon from '@material-ui/icons/Menu';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Slide from '@material-ui/core/Slide';
+
+import Modal from '../../Modal';
 
 
 import style from "./Drawer.module.scss";
 
 export default function Drawer({ openDrawer, toggleDrawer }) {
   return (
-      <div className={style.modal}>
-        <ClickAwayListener onClickAway={toggleDrawer}>
-          <Slide in={true} direction="right">
-            <div className={style.drawer}>
-              <div className={style.title}>
-                <button className={style.menuIcon}><MenuIcon onClick={toggleDrawer}/></button>
-                <div className={style.logo}>
-                  <YouTubeIcon className={style.logo}/>
-                  Youtube
-                </div>
-              </div>
-              <div className={style.item}>
-                <HomeIcon className={style.icon}/>
-                Home
-              </div>
-              <div className={style.item}>
-              <WhatshotIcon className={style.icon}/>
-              Trending
-              </div>
-              <div className={style.item}>
-              <SubscriptionsIcon className={style.icon}/>
-              Subscriptions
-              </div>
-              <div className={style.item}>
-              <VideoLibraryIcon className={style.icon}/>
-              Library
-              </div>
-              <div className={style.item}>
-              <HistoryIcon className={style.icon}/>
-              History
-              </div>
-              <div className={style.item}>
-              <OndemandVideoIcon className={style.icon}/>
-              Your videos
-              </div>
-              <div className={style.item}>
-              <WatchLaterIcon className={style.icon}/>
-              Watch later
-              </div>
-              <div className={style.item}>
-              <PlaylistPlayIcon className={style.icon}/>
-              Playlist
-              </div>
-              <div className={style.item}>
-              <ExpandMoreIcon className={style.icon}/>
-              Show more
-              </div>
-              <div className={style.item}>
-              <ExpandMoreIcon className={style.icon}/>
-              Show more
-              </div>
-              <div className={style.item}>
-              <ExpandMoreIcon className={style.icon}/>
-              Show more
-              </div>
-              <div className={style.item}>
-              <ExpandMoreIcon className={style.icon}/>
-              Show more
-              </div>
-              <div className={style.item}>
-              <ExpandMoreIcon className={style.icon}/>
-              Show more
-              </div>
-              <div className={style.item}>
-              <ExpandMoreIcon className={style.icon}/>
-              Show more
-              </div>
+    <Modal toggle={toggleDrawer}>
+      <Slide in={true} direction="right">
+        <div className={style.drawer}>
+          <div className={style.title}>
+            <button className={style.menuIcon}><MenuIcon onClick={toggleDrawer}/></button>
+            <div className={style.logo}>
+              <YouTubeIcon className={style.logo}/>
+              Youtube
             </div>
-          </Slide>
-        </ClickAwayListener>
-      </div>
+          </div>
+          <div className={style.item}>
+            <HomeIcon className={style.icon}/>
+            Home
+          </div>
+          <div className={style.item}>
+          <WhatshotIcon className={style.icon}/>
+          Trending
+          </div>
+          <div className={style.item}>
+          <SubscriptionsIcon className={style.icon}/>
+          Subscriptions
+          </div>
+          <div className={style.item}>
+          <VideoLibraryIcon className={style.icon}/>
+          Library
+          </div>
+          <div className={style.item}>
+          <HistoryIcon className={style.icon}/>
+          History
+          </div>
+          <div className={style.item}>
+          <OndemandVideoIcon className={style.icon}/>
+          Your videos
+          </div>
+          <div className={style.item}>
+          <WatchLaterIcon className={style.icon}/>
+          Watch later
+          </div>
+          <div className={style.item}>
+          <PlaylistPlayIcon className={style.icon}/>
+          Playlist
+          </div>
+          <div className={style.item}>
+          <ExpandMoreIcon className={style.icon}/>
+          Show more
+          </div>
+          <div className={style.item}>
+          <ExpandMoreIcon className={style.icon}/>
+          Show more
+          </div>
+          <div className={style.item}>
+          <ExpandMoreIcon className={style.icon}/>
+          Show more
+          </div>
+          <div className={style.item}>
+          <ExpandMoreIcon className={style.icon}/>
+          Show more
+          </div>
+          <div className={style.item}>
+          <ExpandMoreIcon className={style.icon}/>
+          Show more
+          </div>
+          <div className={style.item}>
+          <ExpandMoreIcon className={style.icon}/>
+          Show more
+          </div>
+        </div>
+      </Slide>
+    </Modal>
   );
 }
